2c5d72629d2d59dfe37eab13ef07d6ffe1f86da4
Shmuel Shif <shmulikshif@gmail.com>
2026-05-24 20:55:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -34,6 +34,5 @@
   setupModal('.link-item.kosher', 'kosherModal', 'kosherModalClose');
   setupModal('.link-item.whatsapp', 'whatsappModal', 'whatsappModalClose');
   setupModal('.link-item.site', 'siteModal', 'siteModalClose');
-  setupModal('.link-item.delivery', 'deliveryModal', 'deliveryModalClose');
   setupModal('.link-item.tenbis', 'tenbisModal', 'tenbisModalClose');
 })();
